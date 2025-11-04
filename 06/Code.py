8780f262a41fc27a7431817ee8875835b5b34272
aviel12 <aviel.amselem@mail.huji.ac.il>
2025-11-04 13:46:35 +0200

    Update Code

--- 06/Code.py
+++ 06/Code.py
@@ -20,7 +20,17 @@ class Code:
             str: 3-bit long binary code of the given mnemonic.
         """
         # Your code goes here!
-        pass
+        dest_table = {
+            'null': '000',
+            'M': '001',
+            'D': '010',
+            'MD': '011',
+            'A': '100',
+            'AM': '101',
+            'AD': '110',
+            'AMD': '111'
+        }
+        return dest_table.get(mnemonic, '000')  # Default to '000'  if mnemonic not found   
 
     @staticmethod
     def comp(mnemonic: str) -> str:
@@ -32,7 +42,38 @@ class Code:
             str: the binary code of the given mnemonic.
         """
         # Your code goes here!
-        pass
+        comp_table = {
+            '0': '0101010',
+            '1': '0111111',
+            '-1': '0111010',
+            'D': '0001100',
+            'A': '0110000',
+            '!D': '0001101',
+            '!A': '0110001',
+            '-D': '0001111',
+            '-A': '0110011',
+            'D+1': '0011111',
+            'A+1': '0110111',
+            'D-1': '0001110',
+            'A-1': '0110010',
+            'D+A': '0000010',
+            'D-A': '0010011',
+            'A-D': '0000111',
+            'D&A': '0000000',
+            'D|A': '0010101',
+            'M': '1110000',
+            '!M': '1110001',
+            '-M': '1110011',
+            'M+1': '1110111',
+            'M-1': '1110010',
+            'D+M': '1000010',
+            'D-M': '1010011',
+            'M-D': '1000111',
+            'D&M': '1000000',
+            'D|M': '1010101'
+        } 
+        return comp_table.get(mnemonic, '0000000')  # Default to '0000000' if mnemonic not found 
+      
 
     @staticmethod
     def jump(mnemonic: str) -> str:
@@ -44,4 +85,14 @@ class Code:
             str: 3-bit long binary code of the given mnemonic.
         """
         # Your code goes here!
-        pass
+        jump_table = {
+            'null': '000',
+            'JGT': '001',
+            'JEQ': '010',
+            'JGE': '011',
+            'JLT': '100',
+            'JNE': '101',
+            'JLE': '110',
+            'JMP': '111'
+        }
+        return jump_table.get(mnemonic, '000')  # Default to '000' if mnemonic not found
